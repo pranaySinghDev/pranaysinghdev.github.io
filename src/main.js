8ce8efb1993688c1ca340a9d9ca010230c412fc6
pranaysinghdev <pranaysinghdev@gmail.com>
2020-07-27 00:49:38 +0530

    Moved svelte project to root

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import App from './App.svelte';
-import dataset from './../../js/constants.json';
+import dataset from '../js/constants.json';
 const app = new App({
 	target: document.body,
 	props: {
